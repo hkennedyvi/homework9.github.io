d39cdfebedde9ecb5cf154bab39ac7038a11b697
Howard Kennedy <hkennedyvi@gmail.com>
2019-12-29 17:00:16 -0800

    inquirer added to collect user data. Initial json package created

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+//const util = require("util");
+
+inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your github user name?"
+        },
+        {
+            type: "list",
+            name: "color",
+            choices: [
+                "green",
+                "blue",
+                "pink",
+                "red"
+            ]
+        }
+    ]).then(function(data) {
+        var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!");
+
+  });
+});
+
+
+
+
+
+
+/*
 const questions = [
   
 ];
@@ -9,3 +49,4 @@ function writeToFile(fileName, data) {
 function init() {
 
 init();
+*/
