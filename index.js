d43286f7786a3b17f4a5f50037293a55e31c34a1
Howard Kennedy <hkennedyvi@gmail.com>
2019-12-31 18:33:26 -0800

    variables created to store applicant data, file writing function added

--- index.js
+++ index.js
@@ -3,56 +3,62 @@ const fs = require("fs");
 const axios = require("axios");
 
 inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your github user name?"
-        },
-        {
-            type: "list",
-            name: "color",
-            choices: [
-                "green",
-                "blue",
-                "pink",
-                "red"
-            ]
-        }
-    ]).then(function({ name }) {
-        const queryUrl = `https://api.github.com/users/${name}/repos?per_page=100`;
+    {
+        type: "input",
+        name: "name",
+        message: "What is your github user name?"
+    },
+    {
+        type: "list",
+        name: "color",
+        choices: [
+            "green",
+            "blue",
+            "pink",
+            "red"
+        ]
+    }
+]).then(function ({ name }) {
+    const queryUrl = `https://api.github.com/users/${name}`;
 
-        axios.get(queryUrl).then(function(results) {
-            const repoNames = results.data.map(function(repo) {
-                return repo.name;
-            });
+    axios.get(queryUrl).then(function (results) {
+        const appName = results.data.name;
+        const appLocation = results.data.location;
+        const appBio = results.data.bio;
+        const appBlog = results.data.blog;
+        const appProfile = results.data.url;
+        const appRepos = results.data.public_repos;
+        const appFollowers = results.data.followers;
+        const appFollowing = results.data.following;
 
-            const repoNamesStr = repoNames.join("\n");
+        const fullApp = [appName, + "\n" + appLocation, + "\n" + appBio,
+                        appBlog, + "\n" + appProfile, + "\n" + appRepos,
+                        appFollowers, + "\n" + appFollowing];
+        console.log(fullApp);
 
-            fs.writeFile("repos.txt", repoNamesStr, function(err) {
-                if (err) {
-                    throw err;
-                }
-
-                console.log(`${repoNames}`);
-            });
+        fs.writeFile("application.txt", fullApp, function (err) {
+            if (err) {
+                throw err;
+            }
         });
     });
-  
-  
-  
-        
+});
+
+
 
 
 /*
+
+
 const questions = [
-  
+
 ];
 
 function writeToFile(fileName, data) {
- 
+
 }
 
 function init() {
 
-init();
-*/
+    init();
+};*/
