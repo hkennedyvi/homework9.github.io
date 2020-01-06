663075583fa136346fdc906958220bf7019e2105
Howard Kennedy <hkennedyvi@gmail.com>
2020-01-05 19:20:48 -0800

    generateHTML script file exported to index script file, write html file working

--- index.js
+++ index.js
@@ -1,54 +1,79 @@
+const { generateHTML } = require("./generateHTML");
 const inquirer = require("inquirer");
-const fs = require('fs');
-   /* convertFactory = require('electron-html-to');
- const conversion = convertFactory({
-  converterPath: convertFactory.converters.PDF
+const fs = require("fs");
+const util = require("util");
+/* convertFactory = require('electron-html-to');
+const conversion = convertFactory({
+converterPath: convertFactory.converters.PDF
 });*/
 const axios = require("axios");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 inquirer.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your github user name?"
-    },
-    {
-        type: "list",
-        name: "color",
-        choices: [
-            "green",
-            "blue",
-            "pink",
-            "red"
-        ]
+  {
+    type: "input",
+    name: "name",
+    message: "What is your github user name?"
+  },
+  {
+    type: "list",
+    name: "color",
+    choices: [
+      "green",
+      "blue",
+      "pink",
+      "red"
+    ]
+  }
+]).then(function ({ name, color }) {
+  const queryUrl = `https://api.github.com/users/${name}`;
+
+  axios.get(queryUrl).then(function (results) {
+    const appName = results.data.name;
+    const appLocation = results.data.location;
+    const appBio = results.data.bio;
+    const appBlog = results.data.blog;
+    const appProfile = results.data.url;
+    const appRepos = results.data.public_repos;
+    const appFollowers = results.data.followers;
+    const appFollowing = results.data.following;
+
+    const data = {
+      appName: appName,
+      appLocation: appLocation,
+      color: color,
     }
-]).then(function ({ name }) {
-    const queryUrl = `https://api.github.com/users/${name}`;
 
-    axios.get(queryUrl).then(function (results) {
-        const appName = results.data.name;
-        const appLocation = results.data.location;
-        const appBio = results.data.bio;
-        const appBlog = results.data.blog;
-        const appProfile = results.data.url;
-        const appRepos = results.data.public_repos;
-        const appFollowers = results.data.followers;
-        const appFollowing = results.data.following;
+    const html = generateHTML(data);
+    // example of use data.appName
 
-        const fullApp = [appName, + "\n" + appLocation, + "\n" + appBio,
-                        appBlog, + "\n" + appProfile, + "\n" + appRepos,
-                        appFollowers, + "\n" + appFollowing];
-        console.log(fullApp);
+    const fullApp = [appName, + "\n" + appLocation, + "\n" + appBio,
+      appBlog, + "\n" + appProfile, + "\n" + appRepos,
+      appFollowers, + "\n" + appFollowing];
+    console.log(fullApp);
+    console.log(color);
+    console.log(html);
 
-        
-
-        fs.writeFile("application.txt", fullApp, function (err) {
-            if (err) {
-                throw err;
-            }
-        });
+    return writeFileAsync("index.html", html);
+    /*
+    fs.writeFile("application.txt", fullApp, function (err) {
+      if (err) {
+        throw err;
+      }
     });
-});
+    */
+  });
+})/*.then(function ({color}) {
+  console.log("Logging data ", data);
+  const html = generate.generateHTML(data);
+
+  return writeFileAsync("index.html", html);
+})
+  .catch(function (err) {
+    console.log(err);
+  });
+*/
+
 
 
 
